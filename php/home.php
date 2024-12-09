e6109fc0a14e18e599c04ba59bf28149bbc8f3df
Navid Dezashibi <dezashibi@gmail.com>
2024-12-09 13:22:53 +0400

    add icon

--- php/home.php
+++ php/home.php
@@ -40,7 +40,16 @@
 								<?php echo $page->date() ?>
 							</time>
 							-
-							<i class="icon icon-clock"></i><?php echo $L->get('Reading time') . ': ' . $page->readingTime(); ?>
+							<i>
+							  <svg width="144px" height="144px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+							    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+							    <g id="SVGRepo_iconCarrier">
+							      <path d="M12 7V12H15M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+							    </g>
+							  </svg>
+							</i>
+							<?php echo $L->get('Reading time') . ': ' . $page->readingTime(); ?>
 						</span>
 					</div>
 					<h2 class="entry-title title-font text-italic">
