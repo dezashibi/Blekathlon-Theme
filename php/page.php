469266850f1a19985f91e960ad891c2404221a92
Navid Dezashibi <dezashibi@gmail.com>
2024-12-09 13:49:21 +0400

    remove newline

--- php/page.php
+++ php/page.php
@@ -14,7 +14,7 @@
 					<span><?php echo $L->get('Edit'); ?></span>
 				</a>
 				<?php endif; ?> -->
-				<br/><i>
+				<i>
 				  <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 				    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
 				    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
