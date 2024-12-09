14b3c19a7713885e9bad6f7fd662ae9c2f7ba418
Navid Dezashibi <dezashibi@gmail.com>
2024-12-09 13:48:41 +0400

    disable edit

--- php/page.php
+++ php/page.php
@@ -6,14 +6,14 @@
 				<h1 class="entry-title title-font text-italic">
 					<?php echo $page->title(); ?>
 				</h1>
-				<?php if($login->isLogged()) if($canEdit = checkRole(array('admin', 'editor'))):?>
+				<!-- <?php if($login->isLogged()) if($canEdit = checkRole(array('admin', 'editor'))):?>
 				<a href="<?php echo HTML_PATH_ADMIN_ROOT.'edit-content/'.$page->slug() ?>" style="float:right" target="_blank">
 					<svg class="icon" viewBox="0 0 32 32">
 						<use xlink:href="#icon-pencil"></use>
 					</svg>
 					<span><?php echo $L->get('Edit'); ?></span>
 				</a>
-				<?php endif; ?>
+				<?php endif; ?> -->
 				<br/><i>
 				  <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 				    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
