31d62c75feadc785aec04e933684c2700ead6734
Navid Dezashibi <dezashibi@gmail.com>
2024-12-09 13:47:20 +0400

    add reading time to page

--- php/page.php
+++ php/page.php
@@ -14,6 +14,15 @@
 					<span><?php echo $L->get('Edit'); ?></span>
 				</a>
 				<?php endif; ?>
+				<br/><i>
+				  <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+				    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+				    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+				    <g id="SVGRepo_iconCarrier">
+				      <path d="M12 7V12H15M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#6c6c6c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+				    </g>
+				  </svg>
+				</i>
 			</header>
 			<?php if ($page->coverImage()): ?>
 			<div class="entry-media post-thumbnail post-thumbnail-singular">
