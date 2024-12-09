75515f0b136fd153d008b2873d685a0705182f1a
Navid Dezashibi <dezashibi@gmail.com>
2024-12-09 14:21:08 +0400

    remove reading time for static pages

--- php/page.php
+++ php/page.php
@@ -14,6 +14,7 @@
 					<span><?php echo $L->get('Edit'); ?></span>
 				</a>
 				<?php endif; ?> -->
+				<?php if(!$page->isStatic()):?>
 				<i>
 				  <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 				    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -23,6 +24,7 @@
 				    </g>
 				  </svg>
 				</i><?php echo $L->get('Reading time') . ': ' . $page->readingTime(); ?>
+				<?php endif; ?>
 			</header>
 			<?php if ($page->coverImage()): ?>
 			<div class="entry-media post-thumbnail post-thumbnail-singular">
