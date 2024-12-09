898ffe70a74c21e0e734c887263df069a8625ef4
Navid Dezashibi <dezashibi@gmail.com>
2024-12-09 13:29:35 +0400

    make icon smaller

--- php/home.php
+++ php/home.php
@@ -41,7 +41,7 @@
 							</time>
 							-
 							<i>
-							  <svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							  <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
 							    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
 							    <g id="SVGRepo_iconCarrier">
